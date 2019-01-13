d1540fe457d7b01ac2f2f8238749980e4e6278da
dmytro.kushnir <dimakush1@gmail.com>
2019-01-13 16:03:02 +0200

    new icon init

--- App.js
+++ App.js
@@ -40,10 +40,6 @@ export default class App extends React.Component {
 
   _loadResourcesAsync = async () => {
     return Promise.all([
-      Asset.loadAsync([
-        require('./src/assets/images/robot-dev.png'),
-        require('./src/assets/images/robot-prod.png')
-      ]),
       Font.loadAsync({
         // This is the font that we are using for our tab bar
         ...Icon.Ionicons.font,
